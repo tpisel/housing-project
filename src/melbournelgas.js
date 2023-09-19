b531e1ce8abb581b88c75a54ecba51ee59d86a9a
RJAndromeda <133020344+RJAndromeda@users.noreply.github.com>
2023-09-20 06:34:19 +1000

    amending melbournelgas view

--- src/melbournelgas.js
+++ src/melbournelgas.js
@@ -6,45 +6,47 @@
 
  // Create the base layers.
  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors <a href="https://data.gov.au/dataset/ds-dga-bdf92691-c6fe-42b9-a0e2-a4cd716fa811/details">VIC Local Government Areas - Geoscape Administrative Boundaries</a>'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors <a href="https://data.gov.au/dataset/ds-dga-bdf92691-c6fe-42b9-a0e2-a4cd716fa811/details">VIC Local Government Areas - Geoscape Administrative Boundaries</a> <a href="https://github.com/davidmerfield/randomColor">RandomColours generated the colours on the map</a>'
 }).addTo(myMap);
 
 
 // Load the GeoJSON data for the LGA mapping area.
 let link = "https://data.gov.au/geoserver/vic-local-government-areas-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_bdf92691_c6fe_42b9_a0e2_a4cd716fa811&outputFormat=json";
 
-// function to set the colors of the LGAs ( from https://colorbrewer2.org/#type=qualitative&scheme=Set3&n=10)
+// function to set the colors of the LGAs (using randomColour js library)
+var randomColor = require('randomcolor');
+
 function chooseColor(vic_lga__3) {
-  if (vic_lga__3 == "GLEN EIRA") return "yellow";
-  else if (vic_lga__3 == "BAYSIDE") return "blue";
-  else if (vic_lga__3 == "MELBOURNE") return "red";
-  else if (vic_lga__3 == "PORT PHILLIP") return "purple";
-  else if (vic_lga__3 == "GREATER DANDENONG") return "#ff7f00";
-  else if (vic_lga__3 == "KINGSTON") return "orange";
-  else if (vic_lga__3 == "BOROONDARA") return "#a6cee3";
-  else if (vic_lga__3 == "CASEY") return "#33a02c";
-  else if (vic_lga__3 == "FRANKSTON") return "#33a04b";
-  else if (vic_lga__3 == "STONNINGTON") return "#1f78b4";
-  else if (vic_lga__3 == "DAREBIN") return "#ff7f00";
-  else if (vic_lga__3 == "HUME") return "#fb9a99";
-  else if (vic_lga__3 == "HOBSONS BAY") return "#fb9a92";
-  else if (vic_lga__3 == "KNOX") return "#6a3d9a";
-  else if (vic_lga__3 == "MANNINGHAM") return "#e31a1c";
-  else if (vic_lga__3 == "BANYULE") return "#fb9a99";
-  else if (vic_lga__3 == "CARDINIA") return "#b2df8a";
-  else if (vic_lga__3 == "BRIMBANK") return "#fdbf6f";
-  else if (vic_lga__3 == "MARIBYRNONG") return "#ffffb3";
-  else if (vic_lga__3 == "MAROONDAH") return "#b3de69";
-  else if (vic_lga__3 == "MORELAND") return "#9dcde8";
-  else if (vic_lga__3 == "MELTON") return "#8dd3c7";
-  else if (vic_lga__3 == "MONASH") return "#b3de69";
-  else if (vic_lga__3 == "MOONEE VALLEY") return "#ff7f00";
-  else if (vic_lga__3 == "MORNINGTON PENINSULA") return "#ff7f50";
-  else if (vic_lga__3 == "WHITTLESEA") return "#fb9222";
-  else if (vic_lga__3 == "WHITEHORSE") return "#fb8702";
-  else if (vic_lga__3 == "WYNDHAM") return "#fb8072";
-  else if (vic_lga__3 == "YARRA") return "#fccde5";
-  else if (vic_lga__3 == "YARRA RANGES") return "";
+  if (vic_lga__3 == "GLEN EIRA") return "return randomColor()";
+  else if (vic_lga__3 == "BAYSIDE") return "return randomColor()";
+  else if (vic_lga__3 == "MELBOURNE") return "return randomColor()";
+  else if (vic_lga__3 == "PORT PHILLIP") return "return randomColor()";
+  else if (vic_lga__3 == "GREATER DANDENONG") return "#return randomColor()";
+  else if (vic_lga__3 == "KINGSTON") return "return randomColor()";
+  else if (vic_lga__3 == "BOROONDARA") return "#return randomColor()";
+  else if (vic_lga__3 == "CASEY") return "#return randomColor()";
+  else if (vic_lga__3 == "FRANKSTON") return "#return randomColor()";
+  else if (vic_lga__3 == "STONNINGTON") return "#return randomColor()";
+  else if (vic_lga__3 == "DAREBIN") return "#return randomColor()";
+  else if (vic_lga__3 == "HUME") return "#return randomColor()";
+  else if (vic_lga__3 == "HOBSONS BAY") return "#return randomColor()";
+  else if (vic_lga__3 == "KNOX") return "#return randomColor()";
+  else if (vic_lga__3 == "MANNINGHAM") return "#return randomColor()";
+  else if (vic_lga__3 == "BANYULE") return "#return randomColor()";
+  else if (vic_lga__3 == "CARDINIA") return "#return randomColor()";
+  else if (vic_lga__3 == "BRIMBANK") return "#return randomColor()";
+  else if (vic_lga__3 == "MARIBYRNONG") return "#return randomColor()";
+  else if (vic_lga__3 == "MAROONDAH") return "#return randomColor()";
+  else if (vic_lga__3 == "MORELAND") return "#return randomColor()";
+  else if (vic_lga__3 == "MELTON") return "#return randomColor()";
+  else if (vic_lga__3 == "MONASH") return "#return randomColor()";
+  else if (vic_lga__3 == "MOONEE VALLEY") return "#return randomColor()";
+  else if (vic_lga__3 == "MORNINGTON PENINSULA") return "#return randomColor()";
+  else if (vic_lga__3 == "WHITTLESEA") return "#return randomColor()";
+  else if (vic_lga__3 == "WHITEHORSE") return "#return randomColor()";
+  else if (vic_lga__3 == "WYNDHAM") return "#return randomColor()";
+  else if (vic_lga__3 == "YARRA") return "#return randomColor()";
+  else if (vic_lga__3 == "YARRA RANGES") return "return randomColor()";
   else return "black";
 }
 
